cc06d6f0150a4d05fdb64e07d27f121e36e43639
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2022-02-12 17:29:51 +0200

    update

--- api/app.js
+++ api/app.js
@@ -6,8 +6,17 @@ const app = express();
 const apiKey = "5236a567a3774677b151f9ab897c37a0";
 
 app.get("/top-headlines", async (req, res) => {
-  // res.set("Access-Control-Allow-Origin", "*");
-  const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=5236a567a3774677b151f9ab897c37a0`;
+  res.set("Access-Control-Allow-Origin", "*");
+  const apiUrl = `https://newsapi.org/v2/top-headlines?country=sa&apiKey=${apiKey}`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  res.json(data);
+});
+
+app.get("/everything/:searchTerm", async (req, res) => {
+  const searchTerm = req.params.searchTerm
+  res.set("Access-Control-Allow-Origin", "*");
+  const apiUrl = `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=${apiKey}`;
   const response = await fetch(apiUrl);
   const data = await response.json();
   res.json(data);
